1cbb5ada9632b46deef39ffde725c2e6a8b5e147
Storm_L <648417473@qq.com>
2023-03-13 11:45:00 +0800

    fix bugs

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,5 +2,5 @@ export const environment = {
   'production': false,
   'backend-url': 'http://127.0.0.1:8080',
   'github-redirect-url': 'https://127.0.0.1:4200/login/process',
-  'github-login-url': 'http://127.0.0.1:4300/login/oauth/access_token'
+  'github-login-url': 'https://127.0.0.1:4300/login/oauth/access_token'
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   'production': true,
   'backend-url': 'http://8.219.238.255:8080',
   'github-redirect-url': 'https://8.219.238.255/login/process',
-  'github-login-url': 'http://8.219.238.255:4300/login/oauth/access_token'
+  'github-login-url': 'https://8.219.238.255:4300/login/oauth/access_token'
 }
